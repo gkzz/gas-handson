b1e990b1384f3d26b1eb0b6d9605dc177330e4d1
gkzz <gakuji.tamaki@gmail.com>
2019-03-30 21:45:06 +0900

    debug demo

--- sendRichMsg.js
+++ sendRichMsg.js
@@ -62,6 +62,50 @@ function main() {
     const SS = SpreadsheetApp.getActiveSpreadsheet();
     //const SHEET = SS.getSheetByName('<SheetName e.g. sheet1>');
     const SHEET = SS.getSheetByName(SHEET_NAME);
-    Logger.log('SHEET: %s', SHEET);
-    return;
+    //Logger.log('SHEET: %s', SHEET);
+    //return;
+
+    // Get data
+    const VALUE_RANGE = getValueRange(SHEET);
+
+    //Get data by 5 times
+    //var writeConts = 1;
+    for ( var i = 0; i < VALUE_RANGE.length; i++ ) {
+        
+        Logger.log('writeConts: %s', writeConts);
+        /*
+        if ( writeConts >= 6 ) {
+            break;
+        }
+        */
+       
+        var value = VALUE_RANGE[i];
+        if ( !value ) {
+            Logger.log('Faliled!');
+            Logger.log('i: %s', i);
+            Logger.log('value: %s', value);
+        }
+        Logger.log('Successful!');
+        Logger.log('i: %s', i);
+        Logger.log('value: %s', value);
+        return;
+
+        //writeConts ++;
+    }
+}
+
+function getValueRange(SHEET) {
+    //If you would like to get 1st column...
+    //const OFFSET_ROW = 0;
+    const OFFSET_ROW = 1;
+    const OFFSET_COLUMN = 0;
+    //var LastRowOfA = getLastRowInColumn(SHEET, 1);
+    //var LastColumn = ss.getLastColumn();
+    //var ValueRange = SHEET.getDataRange(top, left, LastRowOfA - top, LastColumn -left).getValues();
+    //var dataRange = SHEET.getDataRange();
+    var valueRange = SHEET.getDataRange()
+        .offset(OFFSET_ROW, OFFSET_COLUMN)
+        .getValues();  
+    Logger.log('ValueRange.length: %s', ValueRange.length); //92
+    return valueRange;
 }
